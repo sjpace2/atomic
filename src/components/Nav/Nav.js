004c9796180fd781dd4993ae049472001b306ed7
Stephen Pace <sjpace2@Stephens-iMac.local>
2018-09-05 21:19:02 -0600

    navbar styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
+import justAtomic from './../../images/just atomic.png';
+import justLogo from './../../images/just logo.png';
 
 class Nav extends Component {
     render(){
         return(
-        <div> 
-            Men Women Kids
+        <div className='navbar'> 
+            <div className='navbar-left'>
+                <img className='nav-logo' src={justLogo} alt="logo"/>
+                <i className="fas fa-bars"></i>
+                <img className='nav-atomic' src={justAtomic} alt="atomic"/>
+            </div>
+            <div className='navbar-right'>
+            <ul className='navbar-menu'>
+               <li>Men</li>
+               <li>Women</li>
+               <li>Kids</li>
+               <li>Athletes</li>
+               <li>Atomic World</li>
+            </ul>
+            <i className="fas fa-search"></i>
+            </div>
         </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {HashRouter} from 'react-router-dom';
+import './Main.css';
 
 
 //import registerServiceWorker from './registerServiceWorker';
