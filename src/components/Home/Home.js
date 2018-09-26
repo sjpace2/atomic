99984b035fb50b0796101460ad0df7bd03aa8e6f
Stephen Pace <sjpace2@Stephens-iMac.local>
2018-09-26 11:55:11 -0600

    added background pic

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,29 +1,13 @@
 import React, {Component} from 'react';
+import homePic from './../../images/home/bent_chetler_ski_0.jpg';
 
 class Home extends Component {
-    constructor(){
-        super()
-        this.state={
-            number: 1
-        }
-    }
-
-    increaseNumber=()=>{
-        this.setState({
-            number: this.state.number+1
-        })
-    }
-
-
-
-
+   
     render(){
-        console.log(this.state.number)
+       
         return(
-            <div>
-                Home page
-                <button onClick={()=>this.increaseNumber()}>Increase Number!</button>
-                {this.state.number}
+            <div className='home-background'>
+                 <img src={homePic} alt="bent chetler"/> 
             </div>
         )
     }
