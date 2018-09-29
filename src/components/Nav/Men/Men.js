b4c2781d05a044236bcc0b907c8a83807f09c55c
Stephen Pace <sjpace2@Stephens-iMac.local>
2018-09-28 21:07:45 -0600

    more underline work

--- src/components/Nav/Men/Men.js
+++ src/components/Nav/Men/Men.js
@@ -6,15 +6,19 @@ class Men extends Component {
         super()
     }
 
-    changeAlpineBackground=(color, display, textColor, textColor2)=>{
+    changeAlpineBackground=(color, display, textColor, textColor2, underlineColorAlp, ucColorTour)=>{
         document.getElementById('navbar-alpine-special').style.background = color;
         document.getElementById('navbar-drop-alpine-special').style.display = display;
         document.getElementById('navbar-drop-nordic-content-special').style.display = 'none'
         document.getElementById('navbar-alpine-special2').style.color = textColor;
         document.getElementById('navbar-touring-special').style.color = textColor2; 
+        document.getElementById('underline-move-red-alpine').style.background = underlineColorAlp;
+        document.getElementById('underline-move-red-touring').style.display = 'flex';
+        document.getElementById('underline-move-red-touring').style.background = ucColorTour;
+        // document.getElementById('underline-move-red-nordic').style.background = '#CB1F3E';
     }
 
-    changeTouringBackground=(color, display, textColor1, textColor2)=>{
+    changeTouringBackground=(color, display, textColor1, textColor2, ucColorAlp)=>{
         document.getElementById('navbar-alpine-special').style.display = 'flex';
         document.getElementById('navbar-drop-touring-special').style.display = 'flex';
         document.getElementById('navbar-alpine-special').style.background = color;
@@ -23,6 +27,9 @@ class Men extends Component {
         document.getElementById('navbar-drop-nordic-content-special').style.display = 'flex';
         document.getElementById('navbar-nordic-special').style.color = textColor1;
         document.getElementById('navbar-alpine-special2').style.color = textColor2;
+        document.getElementById('underline-move-red-alpine').style.background = ucColorAlp;
+        document.getElementById('underline-move-red-nordic').style.display = 'flex'
+        document.getElementById('underline-move-red-nordic').style.background = '#CB1F3E';
     }
 
     changeNordicBackground=(textColor1)=>{
@@ -31,16 +38,27 @@ class Men extends Component {
         document.getElementById('navbar-alpine-special').style.background = 'white';
         document.getElementById('navbar-drop-alpine-special').style.display = 'flex';
         document.getElementById('navbar-nordic-special').style.color = textColor1;
+        document.getElementById('underline-move-red-nordic').style.background = '#f1f1f1';
+        document.getElementById('underline-move-red-touring').style.background = '#CB1F3E';
     }
 
     underlineMove=()=>{
         document.getElementById('underline-move').style.display = 'flex';
+        document.getElementById('underline-move-red-alpine').style.display = 'flex';
     }
 
     underlineDown=()=>{
         document.getElementById('underline-move').style.display = 'none';
     }
 
+    alpToTour=()=>{
+        document.getElementById('underline-move-red-touring').style.background = '#CB1F3E';
+    }
+    tourToNord=()=>{
+        document.getElementById('underline-move-red-nordic').style.background = '#CB1F3E';
+    }
+
+  
     render(){
         return(
         <div>
@@ -48,10 +66,12 @@ class Men extends Component {
               <div className='underline' id='underline-move'></div>
                 <div className='navbar-dropdown'>
                   <div className='navbar-drop-content'>
-                    <div className='navbar-drop-alpine' id='navbar-alpine-special'>
+                    <div className='navbar-drop-alpine' id='navbar-alpine-special' onMouseLeave={()=>this.alpToTour()}>
                         <div className='navbar-alpine' id='navbar-alpine-special2'>
                             Alpine
+                            <div className='underline-red' id='underline-move-red-alpine'></div>
                         </div>
+                        
                         <div className='navbar-drop-alpine-content' id='navbar-drop-alpine-special'>
                             <div>
                                 <img src={require("./../../../images/men/alpine/menRedSkis.png")} alt="skis"/>
@@ -91,9 +111,10 @@ class Men extends Component {
                             </div>
                         </div>
                     </div>
-                    <div className='navbar-drop-touring' id='navbar-drop-touring-special' onMouseOver={()=>this.changeAlpineBackground('none', 'none', '#333', '#CB1F3E')} onMouseLeave={()=>this.changeAlpineBackground('white', 'flex', '#CB1F3E', '#333')}>
+                    <div className='navbar-drop-touring' id='navbar-drop-touring-special' onMouseOver={()=>this.changeAlpineBackground('none', 'none', '#333', '#CB1F3E', '#f1f1f1')} onMouseLeave={()=>this.changeAlpineBackground('white', 'flex', '#CB1F3E', '#333', '#CB1F3E', '#f1f1f1')}>
                         <div className='navbar-touring' id='navbar-touring-special'>
                             Touring
+                            <div className='underline-red' id='underline-move-red-touring'></div>
                         </div>
                         <div className='navbar-drop-touring-content' id='navbar-drop-touring-content-special'>
                         <div>
@@ -130,9 +151,10 @@ class Men extends Component {
                             </div>
                         </div>
                     </div>
-                    <div className='navbar-drop-nordic' onMouseOver={()=>this.changeTouringBackground('none', 'none', '#CB1F3E', '#333')} onMouseLeave={()=>this.changeNordicBackground('#333')}>
+                    <div className='navbar-drop-nordic' onMouseOver={()=>this.changeTouringBackground('none', 'none', '#CB1F3E', '#333', '#f1f1f1')} onMouseLeave={()=>this.changeNordicBackground('#333')}>
                         <div className='navbar-nordic' id='navbar-nordic-special'>
                             Nordic
+                            <div className='underline-red' id='underline-move-red-nordic'></div>
                         </div>
                         <div className='navbar-drop-nordic-content' id='navbar-drop-nordic-content-special'>
                         <div>
@@ -161,7 +183,6 @@ class Men extends Component {
 
             </div>
           </div>
-          {/* <div className='underline' id='underline-move'></div> */}
         </div>
         )
     }
