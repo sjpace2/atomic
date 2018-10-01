42b880973f6a34ab1496b4f1b9ede4fbaebc3169
Stephen Pace <sjpace2@Stephens-iMac.local>
2018-10-01 17:11:28 -0600

    women component underlines

--- src/components/Nav/Women/Women.js
+++ src/components/Nav/Women/Women.js
@@ -6,37 +6,65 @@ class Women extends Component {
         super()
     }
 
-    changeAlpineBackground=(color, display)=>{
+    changeAlpineBackground=(color, display, textColor, textColor2, underlineColorAlp, ucColorTour)=>{
         document.getElementById('w-navbar-alpine-special').style.background = color;
         document.getElementById('w-navbar-drop-alpine-special').style.display = display;
         document.getElementById('w-navbar-drop-nordic-content-special').style.display = 'none';
         document.getElementById('w-navbar-drop-touring-content-special').style.display = 'flex';
+///////////// copied from men component
+        document.getElementById('w-navbar-touring-special').style.color = textColor2; 
+        document.getElementById('w-underline-move-red-alpine').style.background = underlineColorAlp;
+        document.getElementById('w-underline-move-red-touring').style.display = 'flex';
+        document.getElementById('w-underline-move-red-touring').style.background = ucColorTour;
+///// added unique to women component
+        document.getElementById('w-navbar-alpine-special2').style.color = textColor;
+        document.getElementById('w-underline-move-red-nordic').style.display = 'none';
               
     }
 
-    changeTouringBackground=(color, display)=>{
+    changeTouringBackground=(color, display, textColor1, textColor2, ucColorAlp)=>{
         document.getElementById('w-navbar-alpine-special').style.display = 'flex';
         document.getElementById('w-navbar-drop-touring-special').style.display = 'flex';
         document.getElementById('w-navbar-alpine-special').style.background = color;
         document.getElementById('w-navbar-drop-alpine-special').style.display = display;
         document.getElementById('w-navbar-drop-touring-content-special').style.display = 'none';
         document.getElementById('w-navbar-drop-nordic-content-special').style.display = 'flex';
+///////////// copied from men component
+        document.getElementById('w-navbar-nordic-special').style.color = textColor1;
+        document.getElementById('w-navbar-alpine-special2').style.color = textColor2;
+        document.getElementById('w-underline-move-red-alpine').style.background = ucColorAlp;
+        document.getElementById('w-underline-move-red-nordic').style.display = 'flex'
+        document.getElementById('w-underline-move-red-nordic').style.background = '#CB1F3E';
     }
 
-    changeNordicBackground=()=>{
+    changeNordicBackground=(textColor1)=>{
         document.getElementById('w-navbar-drop-nordic-content-special').style.display = 'none'
         document.getElementById('w-navbar-drop-touring-content-special').style.display = 'flex'
         document.getElementById('w-navbar-alpine-special').style.background = 'white';
         document.getElementById('w-navbar-drop-alpine-special').style.display = 'flex';
+    /////////////// copied from men component
+        document.getElementById('w-navbar-nordic-special').style.color = textColor1;
+        document.getElementById('underline-move-red-nordic').style.background = '#f1f1f1';
+        document.getElementById('underline-move-red-alpine').style.background = '#CB1F3E';
+        document.getElementById('w-navbar-alpine-special2').style.color = '#CB1F3E';
+    /////// added for only for women component
+        document.getElementById('w-underline-move-red-nordic').style.display = 'none';
+        document.getElementById('w-underline-move-red-alpine').style.background = '#CB1F3E';
+
     }
 
 
     underlineMove=()=>{
         document.getElementById('w-underline-move').style.display = 'flex';
+        document.getElementById('w-underline-move-red-alpine').style.display = 'flex';
     }
 
     underlineDown=()=>{
         document.getElementById('w-underline-move').style.display = 'none';
+    }
+
+    tourToNord=()=>{
+        document.getElementById('underline-move-red-nordic').style.background = '#CB1F3E';
     }
 
     render(){
@@ -47,8 +75,9 @@ class Women extends Component {
                 <div className='w-navbar-dropdown'>
                     <div className='w-navbar-drop-content'>
                         <div className='w-navbar-drop-alpine' id='w-navbar-alpine-special'>
-                            <div className='w-navbar-alpine'>
+                            <div className='w-navbar-alpine' id='w-navbar-alpine-special2'>
                                 Alpine
+                                <div className='w-underline-red' id='w-underline-move-red-alpine'></div>
                             </div>
                             <div className='w-navbar-drop-alpine-content' id='w-navbar-drop-alpine-special'>
                                 <div>
@@ -89,9 +118,10 @@ class Women extends Component {
                                 </div>
                             </div>
                         </div>
-                        <div className='w-navbar-drop-touring' id='w-navbar-drop-touring-special' onMouseOver={()=>this.changeAlpineBackground('none', 'none')} onMouseLeave={()=>this.changeAlpineBackground('white', 'flex')}>
-                            <div className='w-navbar-touring' >
+                        <div className='w-navbar-drop-touring' id='w-navbar-drop-touring-special' onMouseOver={()=>this.changeAlpineBackground('none', 'none', '#333', '#CB1F3E', '#f1f1f1', '#CB1F3E')} onMouseLeave={()=>this.changeAlpineBackground('white', 'flex', '#CB1F3E', '#333', '#CB1F3E', '#f1f1f1')}>
+                            <div className='w-navbar-touring' id='w-navbar-touring-special' >
                                 Touring
+                                <div className='w-underline-red' id='w-underline-move-red-touring'></div>
                             </div>
                             <div className='w-navbar-drop-touring-content' id='w-navbar-drop-touring-content-special'>
                             <div>
@@ -128,9 +158,10 @@ class Women extends Component {
                                 </div>
                             </div>
                         </div>
-                        <div className='w-navbar-drop-nordic' onMouseOver={()=>this.changeTouringBackground('none', 'none')} onMouseLeave={()=>this.changeNordicBackground('white', 'flex')}>
-                            <div className='w-navbar-nordic'>
+                        <div className='w-navbar-drop-nordic' onMouseOver={()=>this.changeTouringBackground('none', 'none', '#CB1F3E', '#333', '#f1f1f1')} onMouseLeave={()=>this.changeNordicBackground('#333')}>
+                            <div className='w-navbar-nordic' id='w-navbar-nordic-special'>
                                 Nordic
+                                <div className='w-underline-red' id='w-underline-move-red-nordic'></div>
                             </div>
                             <div className='w-navbar-drop-nordic-content' id='w-navbar-drop-nordic-content-special'>
                             <div>
@@ -158,7 +189,6 @@ class Women extends Component {
                     </div>
                 </div>
             </div>
-            {/* <div className='w-underline' id='w-underline-move'></div> */}
         </div>
         )
     }
